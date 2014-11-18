84ef675e424347902b75e68754549a3746851de5
parkjiyun <jyun38@gmail.com>
2014-11-18 21:47:32 +0900

    ㅋㅈㄷㄹㅈㄷㄹ

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-var bg, tree, cloud, digdac;
+var bg, tree, cloud, digdac,digdac2, fireB, fireBD;
 
 function setup()
 {
@@ -8,6 +8,9 @@ function setup()
     cloud = loadImage("cloud.png");
     digdac = loadImage("digdac.png");
     mario = loadImage("mario.png");
+    digdac2 = loadImage("dicdac2.png"); 
+    fireB = loadImage("fireB.png");
+
 
     noFill();
     noStroke();
@@ -19,6 +22,8 @@ function draw()
 
   image(tree,0,0,800,500);
   image(cloud,0,0,800,500);
-  image(digdac,0,0,800,500);
+  image(digdac,0,0,180,20);
   image(mario, mouseX, mouseY, 44,53);
+  image(fireB, 0,0,490,560);
+
 }
