5eab6130b5644ff960af56053f15b94f90d8e781
parkjiyun <jyun38@gmail.com>
2014-11-19 16:35:32 +0900

    zwerwergxdfyyrt

--- sketch.js
+++ sketch.js
@@ -16,10 +16,12 @@ function setup()
     grassB = loadImage("grassB.png");
     iceB = loadImage("iceB.png");
     mushroom = loadImage("mushroom.png");
-    mushroom2 = loadImage("mushroom2.png")
+    mushroom2 = loadImage("mushroom2.png");
     waterB = loadImage("waterB.png");
-
-
+    fire1 = loadImage("fire1.png");
+    fire2 = loadImage("fire2.png");
+    fire3 = loadImage("fire3.png");
+    fire4 = loadImage("fire4.png");
 
 
     noFill();
@@ -36,7 +38,7 @@ function draw()
   image(grassC, 0,0,800,500);
   image(tree,600,200,105,80);
   image(cloud,0,0,800,500);
-  image(digdac,310,50,89,86);
+  image(digdac,280,50,89,86);
   image(mario, mouseX, mouseY, 44,53);
   image(fireB, 250,300,49,56);
   image(grassB, 0,0,800,500);
@@ -44,4 +46,8 @@ function draw()
   image(mushroom,270,200,42,43);
   image(mushroom2,0,0,800,500);
   image(waterB,0,0,800,500);
+  image(fire1,360,260,55,86);
+  image(fire2,350,260,26,37);
+  image(fire3,320,260,37,61);
+  image(fire4,300,260,23,40);
 }
