ecc5d8b9b95b2d52080d0d56790ff1cb35c4066c
parkjiyun <jyun38@gmail.com>
2014-11-20 15:01:15 +0900

    jh

--- sketch.js
+++ sketch.js
@@ -49,7 +49,7 @@ function draw()
   } 
 
     if (!fireBTouched){
-        fireB(timing * 1000,                         // x좌표
+        image(fireB,timing * 1000,                         // x좌표
         240 + sin(timing*2*PI)*80, // y좌표
         100,                                  // width
         100);   
