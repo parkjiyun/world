1d89fcefd2c6e182926b7fbe4a4e58528e75592d
parkjiyun <jyun38@gmail.com>
2014-11-25 19:12:48 +0900

    35ydfg

--- sketch.js
+++ sketch.js
@@ -45,7 +45,6 @@ function draw()
   background(bg);
   var t1 = (new Date()%6000)/6000;
   var t2 = (new Date()%8000)/8000;
-  var t3 = (new Date()%50000)/10000;
   var tDig = (new Date()%1000)/1000;
   var timing = (new Date()%1000)/1000;
   var ttree = (new Date()%1000)/1000;
@@ -134,7 +133,7 @@ function draw()
     }
 
 
-  image(cloud,10+sin(t3*1)*1000,10,1100,600);
+  image(cloud,1000+sin(t1*1)*100,10,1100,600);
   image(mario, mouseX, mouseY, 44,53);
 
 
