64e1b7a867da1c07cba96dbd2829fc0bac59ce93
parkjiyun <jyun38@gmail.com>
2014-11-25 19:19:32 +0900

    twwetwe

--- sketch.js
+++ sketch.js
@@ -78,9 +78,9 @@ function draw()
 
 //tree2
     if(ttree <0.5){
-    image(tree,800,400,85,60);
+    image(tree,770,400,85,60);
   }else{
-    image(tree2,800,400,85,60);
+    image(tree2,770,400,85,60);
   } 
 
 
